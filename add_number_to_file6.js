cc67be831ddd1794e844a9da718b2ebbc69775f6
ElizPN <pankevych.liza@gmail.com>
2023-03-26 16:10:49 +0200

    change number in text file

--- add_number_to_file6.js
+++ add_number_to_file6.js
@@ -2,6 +2,10 @@ import fs from "fs";
 
 let number = fs.readFileSync("./file6.txt", "utf8");
 
+console.log(typeof number);
+
+number = parseInt(number);
 number += 1;
+number = number.toString();
 
 fs.writeFileSync("./file6.txt", number);
