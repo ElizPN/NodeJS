23e3fc3529dd8f283d466d850310588865ddc815
ElizPN <pankevych.liza@gmail.com>
2023-03-29 14:03:29 +0200

    add error comment

--- async_readFile.js
+++ async_readFile.js
@@ -1,9 +1,9 @@
 import fs from "fs";
 
-fs.readFile("num0.txt", "utf-8", (err, data) => {
+fs.readFile("num3.txt", "utf-8", (err, data) => {
   let num = parseInt(data);
   let square = num * num;
-  console.log(err);
+  console.log(err);// will be throw Error if readeble file does not exist.
 
   console.log(square);
 });
