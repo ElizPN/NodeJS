f1c0152bd5ec53e7e1fdc2ad234fd1e730639880
ElizPN <pankevych.liza@gmail.com>
2023-03-29 13:54:44 +0200

    try to read not existig file

--- async_readFile.js
+++ async_readFile.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-fs.readFile("num3.txt", "utf-8", (err, data) => {
+fs.readFile("num0.txt", "utf-8", (err, data) => {
   let num = parseInt(data);
   let square = num * num;
   console.log(err);
