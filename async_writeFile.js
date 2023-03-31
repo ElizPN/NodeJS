ebccc3e2b9dcc021a2c6ce866256d19fe542c548
ElizPN <pankevych.liza@gmail.com>
2023-03-31 14:26:22 +0200

    chanhe the loop

--- async_writeFile.js
+++ async_writeFile.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
-for (let i = 0; i <= 10; i++) {
-  let str = i.toString()
+for (let i = 1; i <= 10; i++) {
+  let str = i.toString();
   console.log(str);
 
   fs.writeFile(`asyncFile${i}.txt`, str, (err) => {
