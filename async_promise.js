3c6e2f3f1a970fb37bd4e6ec11ecdb0574099a22
ElizPN <pankevych.liza@gmail.com>
2023-04-02 13:46:58 +0200

    fix file name

--- async_promise.js
+++ async_promise.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 fs.promises
-  .readFile("num0000.txt", "utf8")
+  .readFile("num4.txt", "utf8")
   .then((data) => {
     let sum = 0;
     for (let i = 0; i < data.length; i++) {
