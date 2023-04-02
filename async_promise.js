d32eb646a7ac17404f3319eb90d9e55d5ec018c3
ElizPN <pankevych.liza@gmail.com>
2023-04-02 13:46:19 +0200

    error hendling

--- async_promise.js
+++ async_promise.js
@@ -1,10 +1,14 @@
 import fs from "fs";
 
-fs.promises.readFile("num4.txt", "utf8").then((data) => {
+fs.promises
+  .readFile("num0000.txt", "utf8")
+  .then((data) => {
     let sum = 0;
     for (let i = 0; i < data.length; i++) {
-        sum += parseInt(data[i]);  
+      sum += parseInt(data[i]);
     }
-    
-  console.log(sum);
-});
+    console.log(sum);
+  })
+  .catch((err) => {
+    console.log("error reading");
+  });
