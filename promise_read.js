d5e26b6d72c2553a583214a1a0ed8e0a16d6a103
ElizPN <pankevych.liza@gmail.com>
2023-04-03 11:19:13 +0200

    promise reaf file

---
+++ promise_read.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+fs.promises
+  .readFile("num4.txt", "utf8")
+  .then((data) => {
+    let sum = 0;
+    for (let i = 0; i < data.length; i++) {
+      sum += parseInt(data[i]);
+    }
+    console.log(sum);
+  })
+  .catch((err) => {
+    console.log("error reading");
+  });
