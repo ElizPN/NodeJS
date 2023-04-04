d61f2602ecd4af8037bb04f3a71009394a4aba9d
ElizPN <pankevych.liza@gmail.com>
2023-04-04 11:30:41 +0200

    count sum of numbers and write to separate file

--- async_await3.js
+++ async_await3.js
@@ -2,13 +2,22 @@ import fs from "fs";
 
 async function func() {
   try {
+    let sum = 0;
     const fileNames = ["num5.txt", "num6.txt", "num7.txt"];
     for (const fileName of fileNames) {
-      let num = Math.random() * 10;
-      num = num.toString();
+      let num = Math.floor(Math.random() * 100);
 
+      num = num.toString();
       await fs.promises.writeFile(fileName, num);
+
+      let data = await fs.promises.readFile(fileName, "utf8");
+      data = parseInt(data);
+      console.log(typeof data);
+      console.log(typeof sum);
+      sum += data;
     }
+    sum = sum.toString();
+    await fs.promises.writeFile("sum2.txt", sum);
   } catch (error) {
     console.log("reading error");
   }
