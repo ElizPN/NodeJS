675da2bca1001e0d13468fc722b93cf87829d034
ElizPN <pankevych.liza@gmail.com>
2023-04-05 11:40:59 +0200

    check is file exist using try catch

--- check_existing.js
+++ check_existing.js
@@ -2,10 +2,9 @@ import fs from "fs/promises";
 import { constants } from "fs";
 import { text } from "stream/consumers";
 
-fs.access("1.txt", constants.F_OK)
-  .then(() => {
-    console.log("file exists");
-  })
-  .catch(() => {
-    console.error("file does not exist");
-  });
+try {
+  await fs.access("1.txt", constants.F_OK);
+  console.log("file exists");
+} catch (error) {
+  console.error("file does not exist");
+}
