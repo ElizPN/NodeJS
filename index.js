723c2edc0ddfdd7c23a1d107f5eb23987dd97079
ElizPN <pankevych.liza@gmail.com>
2023-04-06 10:01:07 +0200

    send html

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import http from "http";
 
 http
   .createServer((request, response) => {
-    response.statusCode = 404;
-    response.write("Ohh we can not find this page");
+    response.setHeader("Content-Type", "text/html");
+    response.setHeader("Cache-Control", "no-cache");
+    response.statusCode = 200;
+    response.write("<b>hello world</b>");
 
     response.end();
   })
