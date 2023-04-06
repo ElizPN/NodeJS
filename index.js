4bacf746b369df092d3920a99d2350713d63644d
ElizPN <pankevych.liza@gmail.com>
2023-04-06 08:59:34 +0200

    add status 404

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import http from "http";
 
-http.createServer((request, response) => {
-  response.statusCode = 200
-  response.write("Everything is ok");
-  response.write("some_text2");
-  response.end();
-}).listen(3000)
+http
+  .createServer((request, response) => {
+    response.statusCode = 404;
+    response.write("Ohh we can not find this page");
+
+    response.end();
+  })
+  .listen(3000);
