985f808fb5e8872e415869f8c08d1acf32d167be
ElizPN <pankevych.liza@gmail.com>
2023-04-06 13:19:17 +0200

    writeHead

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import http from "http";
 
 http
   .createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html");
-    response.setHeader("Cache-Control", "no-cache");
-    response.statusCode = 200;
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+      "Cache-Control": "no-cache",
+    });
+
     const date = new Date();
     const formattedData = date.toLocaleString();
     response.write(`<p>${formattedData}</p>`);
