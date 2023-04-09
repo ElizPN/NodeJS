8a02fa7b652e771aa2481e44fc2a60996822b50e
ElizPN <pankevych.liza@gmail.com>
2023-04-09 12:55:29 +0200

    auto responce ( not competed)

---
+++ auto_response.js
@@ -0,0 +1,30 @@
+import http, { request } from "http";
+import fs from "fs";
+
+http
+  .createServer(async (request, response) => {
+    let status = 404;
+    let data;
+    let type = "text/html";
+    let text = "page is not fond";
+
+    if (request.url === "/my_page") {
+      data = await fs.promises.readFile("my_page.html", "utf8");
+      status = 200;
+    }
+
+    if (request.url === "/img.png") {
+      data = await fs.promises.readFile("img.png");
+      type = "image/png";
+    }
+
+    if (request.url === "/styles.css") {
+      data = await fs.promises.readFile("styles.css");
+      type = "text/css";
+    }
+
+    response.writeHead(status, { "Content-Type": type });
+    response.write(data || text);
+    response.end();
+  })
+  .listen(3000);
