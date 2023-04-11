f1cd64c689451f036ee7897a6db395965b0d6558
ElizPN <pankevych.liza@gmail.com>
2023-04-11 10:53:19 +0200

    add function for mime types

--- static_server.js
+++ static_server.js
@@ -1,6 +1,31 @@
 import http from "http";
 import fs from "fs";
 
+function getMimeType(path) {
+  let mimes = {
+    html: "text/html",
+    jpeg: "image/jpeg",
+    jpg: "image/jpeg",
+    png: "image/png",
+    svg: "image/svg+xml",
+    json: "application/json",
+    js: "text/javascript",
+    css: "text/css",
+    ico: "image/x-icon",
+  };
+
+  let exts = Object.keys(mimes);
+  let extReg = new RegExp("\\.(" + exts.join("|") + ")$");
+
+  let ext = path.match(extReg)[1];
+
+  if (ext) {
+    return mimes[ext];
+  } else {
+    return "text/plain";
+  }
+}
+
 http
   .createServer(async (request, response) => {
     let path = "root" + request.url;
@@ -20,7 +45,7 @@ http
       text = await fs.promises.readFile("root/404.html", "utf8");
     }
 
-    response.writeHead(status, { "Content-Type": "text/html" });
+    response.writeHead(status, { "Content-Type": getMimeType(path) });
     response.write(text);
     response.end();
   })
